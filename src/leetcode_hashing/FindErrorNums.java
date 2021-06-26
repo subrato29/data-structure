9389a164f5b83be5059c0f7d5f0f8e9e423540d2
Subrato Sarkar <subratosarkar@Subratos-MacBook-Pro.local>
2021-06-26 13:19:45 -0400

    more programs added

---
+++ src/leetcode_hashing/FindErrorNums.java
@@ -0,0 +1,57 @@
+/**
+ * 645. Set Mismatch
+Easy
+
+1202
+
+460
+
+Add to List
+
+Share
+You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+
+You are given an integer array nums representing the data status of this set after the error.
+
+Find the number that occurs twice and the number that is missing and return them in the form of an array.
+
+Example 1:
+Input: nums = [1,2,2,4]
+Output: [2,3]
+
+Example 2:
+Input: nums = [1,1]
+Output: [1,2]
+ */
+package leetcode_hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindErrorNums {
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static int[] findErrorNums(int[] nums) {
+		Map < Integer, Integer > map = new HashMap();
+        int dup = -1, missing = 1;
+        for (int n: nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        for (int i = 1; i <= nums.length; i++) {
+            if (map.containsKey(i)) {
+                if (map.get(i) == 2)
+                    dup = i;
+            } else {
+                missing = i;  
+            }
+        }
+        return new int[]{dup, missing};
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = {1,2,2,4};
+		System.out.println(findErrorNums(array));
+	}
+
+}
---
+++ src/leetcode_hashing/Intersect.java
@@ -0,0 +1,65 @@
+/**
+ * 350. Intersection of Two Arrays II
+Easy
+
+2395
+
+534
+
+Add to List
+
+Share
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+
+Example 2:
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Explanation: [9,4] is also accepted.
+ */
+package leetcode_hashing;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Intersect {
+
+	public static int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums1.length; i++) {
+            int num = nums1[i];
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < nums2.length; i++) {
+            int key = nums2[i];
+            if (map.containsKey(key)) {
+            	int value = map.get(key);
+                if (value == 0) {
+                    map.remove(key);
+                } else {
+                    list.add(key);
+                    map.put(key, value - 1);
+                }
+            }
+        }
+        int[] result = new int[list.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+	
+	public static void main(String[] args) {
+		int[] nums1 = {4,9,5};
+		int[] nums2 = {9,4,9,8,4};
+		System.out.println(Arrays.toString(intersect(nums1, nums2)));
+	}
+
+}
