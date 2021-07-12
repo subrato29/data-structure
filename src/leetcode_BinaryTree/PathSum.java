8759587785b5592171128e53d56ea91aa9c19e04
Subrato Sarkar <subratosarkar@Subratos-MacBook-Pro.local>
2021-07-12 00:50:26 -0400

    more leetcode problems added

--- src/leetcode_BinaryTree/PathSum.java
+++ src/leetcode_BinaryTree/PathSum.java
@@ -1,26 +1,39 @@
 /**
- * Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+ * 113. Path Sum II
+Medium
+
+3059
+
+88
+
+Add to List
+
+Share
+Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where each path's sum equals targetSum.
 
 A leaf is a node with no children.
+
 Example 1:
-Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
-Output: true
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: [[5,4,11,2],[5,8,4,5]]
 
 Example 2:
 Input: root = [1,2,3], targetSum = 5
-Output: false
+Output: []
 
 Example 3:
 Input: root = [1,2], targetSum = 0
-Output: false
- */
+Output: []
 
+https://www.youtube.com/watch?v=3B5gnrwRmOA
+ */
 package leetcode_BinaryTree;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class PathSum {
-	
+
 	public static class TreeNode {
 		public int val;
 		public TreeNode left;
@@ -31,30 +44,28 @@ public class PathSum {
 		}
 	}
 	
-	public boolean hasPathSum(TreeNode root, int targetSum) {
-        if (root == null && targetSum == 0) {
-            return false;
-        }
-        ArrayList<Integer> listOfSum = new ArrayList<Integer>();
-        hasPathSumHelper (root, 0, listOfSum);
-        return listOfSum.contains(targetSum);
+	public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        ArrayList<List<Integer>> list = new ArrayList<List<Integer>>();
+        pathSumHelper(root, list, new ArrayList<Integer>(), targetSum);
+        return list;
     }
-	
-	public void hasPathSumHelper(TreeNode root, int branchSum, ArrayList<Integer> listOfSum) {
-        if (root == null) {
-           return;
-        }
-        branchSum += root.val;
-        if (root.left == null && root.right == null) {
-            listOfSum.add(branchSum);
+    
+    public void pathSumHelper(TreeNode node, ArrayList<List<Integer>> list, ArrayList<Integer> path, int sum) {
+        if (node == null) {
             return;
         }
-        hasPathSumHelper(root.left, branchSum, listOfSum);
-        hasPathSumHelper(root.right, branchSum, listOfSum);  
+        path.add (node.val);
+        if (sum == node.val && node.left == null && node.right == null) {
+            list.add (path);
+            return;
+        }
+        pathSumHelper(node.left, list, new ArrayList<Integer>(path), sum - node.val);
+        pathSumHelper(node.right, list, new ArrayList<Integer>(path), sum - node.val);
     }
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 	}
 
 }
