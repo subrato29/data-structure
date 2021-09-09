ee8b24392ac299f402c467789df4db446fb71a14
Subrato Sarkar <subratosarkar@Subratos-MacBook-Pro.local>
2021-09-09 16:29:13 -0400

    leetcode array anf string problem added

--- src/leetcode_array/ValidMountainArray.java
+++ src/leetcode_array/ValidMountainArray.java
@@ -8,31 +8,44 @@ There exists some i with 0 < i < A.length - 1 such that:
 A[0] < A[1] < ... A[i-1] < A[i]
 A[i] > A[i+1] > ... > A[A.length - 1]
 
-		Example 1:
+Example 1:
 
-			Input: [2,1]
-			Output: false
-			Example 2:
+Input: [2,1]
+Output: false
+Example 2:
 
-			Input: [3,5,5]
-			Output: false
-			Example 3:
+Input: [3,5,5]
+Output: false
+Example 3:
 
-			Input: [0,3,2,1]
-			Output: true
+Input: [0,3,2,1]
+Output: true
 */
 package leetcode_array;
 
 public class ValidMountainArray {
-    public boolean validMountainArray(int[] A) {
-        if (A == null || A.length < 3) return false;
-        int n = A.length;
-        int i;
-        for (i = 1; i < n && A[i] > A[i - 1]; i++);
-        if (i == 1 || i == n) return false;
-        for (; i < n && A[i] < A[i - 1]; i++);
-        if (i != n) return false;
-
+    public boolean validMountainArray(int[] arr) {
+        int counterIncrease = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] < arr[i]) {
+                counterIncrease++;
+            } else if (arr[i - 1] > arr[i]) {
+                break;
+            } else if (arr[i - 1] == arr[i]) {
+                return false;
+            }
+        }
+        if (counterIncrease == 0) {
+            return false;
+        }
+        if (counterIncrease == arr.length - 1) {
+            return false;
+        }
+        for (int i = counterIncrease; i < arr.length - 1; i++) {
+            if (arr[i] <= arr[i + 1]) {
+                return false;
+            }
+        }
         return true;
     }
     public static void main(String[] args) {
