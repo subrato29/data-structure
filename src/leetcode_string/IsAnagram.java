6709d1f0d541c7b4949528399886f2db4cfdd165
Subrato Sarkar <subratosarkar@Subratos-MacBook-Pro.local>
2021-09-11 13:55:21 -0400

    leetcode IsAnagram problem added

---
+++ src/leetcode_string/IsAnagram.java
@@ -0,0 +1,53 @@
+/**
+ * 242. Valid Anagram
+Easy
+
+3191
+
+176
+
+Add to List
+
+Share
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+ */
+
+package leetcode_string;
+
+import java.util.HashMap;
+
+public class IsAnagram {
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        HashMap < Character, Integer > map = new HashMap < Character, Integer > ();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            if (!map.containsKey(ch) || map.get(ch) == 0) {
+                return false;
+            }
+            map.put(ch, map.get(ch) - 1);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
