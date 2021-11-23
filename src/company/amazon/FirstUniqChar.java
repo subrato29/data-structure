cb6854c9f6cde03f0ebdffdced6284f34e183be6
Subrato Sarkar <subratosarkar@Subratos-MacBook-Pro.local>
2021-11-23 14:19:32 -0500

    adding leetcode prob

---
+++ src/company/amazon/FirstUniqChar.java
@@ -0,0 +1,51 @@
+/**
+ * 387. First Unique Character in a String
+Easy
+
+3958
+
+171
+
+Add to List
+
+Share
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+Example 1:
+Input: s = "leetcode"
+Output: 0
+
+Example 2:
+Input: s = "loveleetcode"
+Output: 2
+
+Example 3:
+Input: s = "aabb"
+Output: -1
+ */
+package company.amazon;
+
+import java.util.HashMap;
+
+public class FirstUniqChar {
+
+    public int firstUniqChar(String s) {
+        HashMap < Character, Integer > map = new HashMap < Character, Integer > ();
+        for (char ch: s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (map.get(ch) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
