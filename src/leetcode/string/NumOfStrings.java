e6513f99cca6700e78cb637e358cb1e6871764ba
subrato29 <subratosarkar29@gmai.com>
2022-01-03 18:10:55 -0500

    leetcode problem added

---
+++ src/leetcode/string/NumOfStrings.java
@@ -0,0 +1,50 @@
+/**
+ * Given an array of strings patterns and a string word, return the number of strings in patterns that exist as a substring in word.
+
+A substring is a contiguous sequence of characters within a string.
+
+Example 1:
+Input: patterns = ["a","abc","bc","d"], word = "abc"
+Output: 3
+Explanation:
+- "a" appears as a substring in "abc".
+- "abc" appears as a substring in "abc".
+- "bc" appears as a substring in "abc".
+- "d" does not appear as a substring in "abc".
+3 of the strings in patterns appear as a substring in word.
+
+Example 2:
+Input: patterns = ["a","b","c"], word = "aaaaabbbbb"
+Output: 2
+Explanation:
+- "a" appears as a substring in "aaaaabbbbb".
+- "b" appears as a substring in "aaaaabbbbb".
+- "c" does not appear as a substring in "aaaaabbbbb".
+2 of the strings in patterns appear as a substring in word.
+
+Example 3:
+Input: patterns = ["a","a","a"], word = "ab"
+Output: 3
+Explanation: Each of the patterns appears as a substring in word "ab".
+ */
+
+package leetcode.string;
+
+public class NumOfStrings {
+
+    public int numOfStrings(String[] patterns, String word) {
+        int counter = 0;
+        for (String string: patterns) {
+            if (word.indexOf(string) != -1) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
