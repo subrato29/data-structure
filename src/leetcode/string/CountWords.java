84cb0da574d8b86c2820d9f155618e24a60f8af8
subrato29 <subratosarkar29@gmai.com>
2022-01-17 12:08:34 -0500

    added leetcode problem

---
+++ src/leetcode/string/CountWords.java
@@ -0,0 +1,67 @@
+/**
+ * 2085. Count Common Words With One Occurrence
+Easy
+
+175
+
+5
+
+Add to List
+
+Share
+Given two string arrays words1 and words2, return the number of strings that appear exactly once in each of the two arrays.
+
+Example 1:
+Input: words1 = ["leetcode","is","amazing","as","is"], words2 = ["amazing","leetcode","is"]
+Output: 2
+Explanation:
+- "leetcode" appears exactly once in each of the two arrays. We count this string.
+- "amazing" appears exactly once in each of the two arrays. We count this string.
+- "is" appears in each of the two arrays, but there are 2 occurrences of it in words1. We do not count this string.
+- "as" appears once in words1, but does not appear in words2. We do not count this string.
+Thus, there are 2 strings that appear exactly once in each of the two arrays.
+
+Example 2:
+Input: words1 = ["b","bb","bbb"], words2 = ["a","aa","aaa"]
+Output: 0
+Explanation: There are no strings that appear in each of the two arrays.
+
+Example 3:
+Input: words1 = ["a","ab"], words2 = ["a","a","a","ab"]
+Output: 1
+Explanation: The only string that appears exactly once in each of the two arrays is "ab".
+ */
+package leetcode.string;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class CountWords {
+
+    public int countWords(String[] words1, String[] words2) {
+        HashMap < String, Integer > map = new HashMap < String, Integer > ();
+        for (String word1: words1) {
+            map.put(word1, map.getOrDefault(word1, 0) + 1);
+        }
+        for (String word2: words2) {
+            if (map.containsKey(word2) && map.get(word2) <= 1) {
+                map.put(word2, map.get(word2) - 1);
+            }
+        }
+        int counter = 0;
+        Iterator < String > itr = map.keySet().iterator();
+        while (itr.hasNext()) {
+            String key = itr.next();
+            if (map.get(key) == 0) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
