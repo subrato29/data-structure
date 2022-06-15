fa501b19e1ad6789abc4c5c090110a65fba0c959
subrato29 <subratosarkar29@gmai.com>
2022-06-15 15:48:36 -0400

    leetcode problem added

---
+++ src/company/facebook/ValidParentheses.java
@@ -0,0 +1,39 @@
+package company.facebook;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParentheses {
+	
+	public static boolean solution(String str) {
+		Map<Character, Character> map = new HashMap<>();
+		map.put('(', ')');
+		map.put('{', '}');
+		map.put('[', ']');
+		
+		Stack<Character> stack = new Stack<>();
+		
+		for (int i = 0; i < str.length(); i++) {
+			char curr = str.charAt(i);
+			
+			if (map.keySet().contains(curr)) {
+				stack.push(curr);
+			} else if (map.values().contains(curr)) {
+				if (!stack.empty() && map.get(stack.peek()) == curr) {
+					stack.pop();
+				} else {
+					return false;
+				}
+			}
+		}
+		return stack.empty();
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(solution("))(("));
+	}
+
+}
---
+++ src/leetcode/easy/Best_Time_to_Buy_and_Sell_Stock_121.java
@@ -0,0 +1,52 @@
+/**
+ * 121. Best Time to Buy and Sell Stock
+Easy
+
+16892
+
+559
+
+Add to List
+
+Share
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Example 2:
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+ */
+package leetcode.easy;
+
+public class Best_Time_to_Buy_and_Sell_Stock_121 {
+
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if (min > prices[i]) {
+                min = prices[i];
+            } else {
+                max = Math.max(max, prices[i] - min);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
