6be62c2190d717f689a2e1851003accdafbe8eba
subrato29 <subratosarkar29@gmai.com>
2022-07-21 12:44:58 -0400

    leetcode problem added

---
+++ src/leetcode/linkedlist/Palindrome_Linked_List_234.java
@@ -0,0 +1,61 @@
+/**
+ * 234. Palindrome Linked List
+Easy
+
+9863
+
+600
+
+Add to List
+
+Share
+Given the head of a singly linked list, return true if it is a palindrome.
+
+Example 1:
+Input: head = [1,2,2,1]
+Output: true
+
+Example 2:
+Input: head = [1,2]
+Output: false
+ */
+package leetcode.linkedlist;
+
+public class Palindrome_Linked_List_234 {
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+        fast = head;
+        while (slow != null) {
+            if (fast.val != slow.val) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
