ab2ad39923b7008435e1e8f60034289a9eb0984f
subrato29 <subratosarkar29@gmai.com>
2022-12-12 14:34:37 -0500

    more problem added

--- src/leetcode/topInterviewQuestions/ListNode.java
+++ src/leetcode/topInterviewQuestions/ListNode.java
@@ -2,10 +2,10 @@ package leetcode.topInterviewQuestions;
 
 public class ListNode {
 
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
     ListNode() {}
-    ListNode(int val) { this.val = val; }
+    public ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 
 }
---
+++ src/leetcode/topInterviewQuestions/MergeTwoSortedLists_21.java
@@ -0,0 +1,27 @@
+package leetcode.topInterviewQuestions;
+
+public class MergeTwoSortedLists_21 {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode tempNode = new ListNode(0);
+        ListNode currNode = tempNode;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tempNode.next = l1;
+                l1 = l1.next;
+            } else {
+                tempNode.next = l2;
+                l2 = l2.next;
+            }
+            tempNode = tempNode.next;
+        }
+        if (l1 != null) {
+            tempNode.next = l1;
+        }
+        if (l2 != null) {
+            tempNode.next = l2;
+        }
+        return currNode.next;
+    }
+
+}
