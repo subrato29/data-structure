aa7690650615a41b670cdaaf7ed71b51d1f6b70c
subrato29 <subratosarkar29@gmai.com>
2022-12-21 19:09:13 -0500

    leetcode problem added

---
+++ src/leetcode/topInterviewQuestions/GroupAnagrams_46.java
@@ -0,0 +1,44 @@
+/**
+ * 49. Group Anagrams
+ * Medium
+ * 13.6K
+ * 402
+ * Companies
+ * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+ *
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+ *
+ * Example 1:
+ * Input: strs = ["eat","tea","tan","ate","nat","bat"]
+ * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ *
+ * Example 2:
+ * Input: strs = [""]
+ * Output: [[""]]
+ *
+ * Example 3:
+ * Input: strs = ["a"]
+ * Output: [["a"]]
+ */
+package leetcode.topInterviewQuestions;
+
+import java.util.*;
+
+public class GroupAnagrams_46 {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, ArrayList<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] arr = str.toCharArray();
+            Arrays.sort (arr);
+            String sorted = new String(arr);
+            if (map.keySet().contains(sorted)) {
+                map.get(sorted).add (str);
+            } else {
+                map.put (sorted, new ArrayList<>(Arrays.asList(str)));
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
+
+}
