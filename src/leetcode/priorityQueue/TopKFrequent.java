aa84cf970472916a5aead196ee40f402dc202e99
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-05-30 15:47:26 -0400

    leetcode 347

--- src/leetcode/priorityQueue/TopKFrequent.java
+++ src/leetcode/priorityQueue/TopKFrequent.java
@@ -1,65 +1,51 @@
 /**
- * 347. Top K Frequent Elements
-Medium
-
-6151
-
-301
-
-Add to List
-
-Share
-Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
-
-Example 1:
-Input: nums = [1,1,1,2,2,3], k = 2
-Output: [1,2]
-
-Example 2:
-Input: nums = [1], k = 1
-Output: [1]
- 
-
-Constraints:
-1 <= nums.length <= 105
-k is in the range [1, the number of unique elements in the array].
-It is guaranteed that the answer is unique.
-*/
+ * 347. Top K Frequent Elements Medium
+ *
+ * 6151
+ *
+ * 301
+ *
+ * Add to List
+ *
+ * Share Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in
+ * any order.
+ *
+ * Example 1: Input: nums = [1,1,1,2,2,3], k = 2 Output: [1,2]
+ *
+ * Example 2: Input: nums = [1], k = 1 Output: [1]
+ *
+ *
+ * Constraints: 1 <= nums.length <= 105 k is in the range [1, the number of unique elements in the array]. It is
+ * guaranteed that the answer is unique.
+ */
 
 package leetcode.priorityQueue;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
 
 public class TopKFrequent {
 
     public int[] topKFrequent(int[] nums, int k) {
-        if (nums.length == 0 || nums == null) {
-            return new int[] {};
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        HashMap < Integer, Integer > map = new HashMap < Integer, Integer > ();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-        PriorityQueue < Integer > maxHeap = new PriorityQueue < Integer > ((key1, key2) -> map.get(key2) - map.get(key1));
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
         maxHeap.addAll(map.keySet());
-
-        ArrayList < Integer > list = new ArrayList < Integer > ();
-        System.out.println(map.get(maxHeap.peek()));
-        while (map.size() != 0 && maxHeap.size() != 0 && list.size() < k) {
-            list.add(maxHeap.poll());
-        }
-        int[] result = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            result[i] = list.get(i);
+        int[] result = new int[k];
+        int counter = 0;
+        while (!maxHeap.isEmpty() && counter < k) {
+            result[counter] = maxHeap.remove();
+            counter++;
         }
         return result;
     }
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-    	
+
     }
 
 }
