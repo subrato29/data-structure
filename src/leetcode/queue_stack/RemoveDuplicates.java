f5482f71db26bf42355bd0960acbf678f8f7d8fe
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-06-02 13:28:08 -0400

    leetcode 1047

--- src/leetcode/queue_stack/RemoveDuplicates.java
+++ src/leetcode/queue_stack/RemoveDuplicates.java
@@ -1,23 +1,21 @@
 /**
  * 1047. Remove All Adjacent Duplicates In String
-
- * You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
-
-We repeatedly make duplicate removals on s until we no longer can.
-
-Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
-
-Example 1:
-Input: s = "abbaca"
-Output: "ca"
-Explanation: 
-For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
-
-Example 2:
-Input: s = "azxxzy"
-Output: "ay"
-
-Ref: https://www.youtube.com/watch?v=I7xTXJ4OfD4
+ *
+ * You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two
+ * adjacent and equal letters and removing them.
+ *
+ * We repeatedly make duplicate removals on s until we no longer can.
+ *
+ * Return the final string after all such duplicate removals have been made. It can be proven that the answer is
+ * unique.
+ *
+ * Example 1: Input: s = "abbaca" Output: "ca" Explanation: For example, in "abbaca" we could remove "bb" since the
+ * letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is
+ * "aaca", of which only "aa" is possible, so the final string is "ca".
+ *
+ * Example 2: Input: s = "azxxzy" Output: "ay"
+ *
+ * Ref: https://www.youtube.com/watch?v=I7xTXJ4OfD4
  */
 
 package leetcode.queue_stack;
@@ -27,13 +25,13 @@ import java.util.Stack;
 public class RemoveDuplicates {
 
     public String removeDuplicates(String s) {
-        Stack < Character > stack = new Stack < Character > ();
+        Stack<Character> stack = new Stack<>();
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (!stack.isEmpty() && stack.peek() == ch) {
+        for (char ch : s.toCharArray()) {
+            if (!stack.empty() && stack.peek() == ch) {
                 stack.pop();
-            } else {
+            }
+            else {
                 stack.push(ch);
             }
         }
