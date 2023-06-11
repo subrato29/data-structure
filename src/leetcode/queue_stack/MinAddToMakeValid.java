e47e68058b86268c323cf1e4f94454265cb22f24
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-06-10 20:49:49 -0400

    leetcode 921

--- src/leetcode/queue_stack/MinAddToMakeValid.java
+++ src/leetcode/queue_stack/MinAddToMakeValid.java
@@ -1,30 +1,22 @@
 /**
  * 921. Minimum Add to Make Parentheses Valid
-
- * Given a string s of '(' and ')' parentheses, we add the minimum number of parentheses ( '(' or ')', and in any positions ) so that the resulting parentheses string is valid.
-
-Formally, a parentheses string is valid if and only if:
-
-It is the empty string, or
-It can be written as AB (A concatenated with B), where A and B are valid strings, or
-It can be written as (A), where A is a valid string.
-Given a parentheses string, return the minimum number of parentheses we must add to make the resulting string valid.
-
-Example 1:
-Input: s = "())"
-Output: 1
-
-Example 2:
-Input: s = "((("
-Output: 3
-
-Example 3:
-Input: s = "()"
-Output: 0
-
-Example 4:
-Input: s = "()))(("
-Output: 4
+ *
+ * Given a string s of '(' and ')' parentheses, we add the minimum number of parentheses ( '(' or ')', and in any
+ * positions ) so that the resulting parentheses string is valid.
+ *
+ * Formally, a parentheses string is valid if and only if:
+ *
+ * It is the empty string, or It can be written as AB (A concatenated with B), where A and B are valid strings, or It
+ * can be written as (A), where A is a valid string. Given a parentheses string, return the minimum number of
+ * parentheses we must add to make the resulting string valid.
+ *
+ * Example 1: Input: s = "())" Output: 1
+ *
+ * Example 2: Input: s = "(((" Output: 3
+ *
+ * Example 3: Input: s = "()" Output: 0
+ *
+ * Example 4: Input: s = "()))((" Output: 4
  */
 
 package leetcode.queue_stack;
@@ -35,30 +27,31 @@ import java.util.Stack;
 
 public class MinAddToMakeValid {
 
-    public int minAddToMakeValid(String input) {
-        Map < Character, Character > map = new HashMap < > ();
+    public static int minAddToMakeValid(String s) {
+        Map<Character, Character> map = new HashMap<>();
         map.put('(', ')');
 
-        Stack < Character > stack = new Stack();
-        int count = 0;
-        for (int i = 0; i < input.length(); i++) {
-            char curr = input.charAt(i);
-            if (map.containsKey(curr)) {
-                stack.push(curr);
-            } else if (map.values().contains(curr)) {
-                if (!stack.empty() && map.get(stack.peek()) == curr) {
+        Stack<Character> stack = new Stack();
+        int countOfUnbalancedClosedParenthesis = 0;
+        for (char ch : s.toCharArray()) {
+            if (map.containsKey(ch)) {
+                stack.push(ch);
+            }
+            else if (map.containsValue(ch)) {
+                if (!stack.empty() && map.get(stack.peek()) == ch) {
                     stack.pop();
-                } else {
-                    count++;
+                }
+                else {
+                    countOfUnbalancedClosedParenthesis++;
                 }
             }
         }
-        return stack.size() + count;
+        return stack.size() + countOfUnbalancedClosedParenthesis;
     }
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-
+        minAddToMakeValid("()");
     }
 
 }
