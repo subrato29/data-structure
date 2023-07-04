ea5b0fbbe355961a59ad3fdd7e3288407d1d7e3e
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-07-04 12:44:25 -0400

    leetcode 1221

--- src/leetcode/string/BalancedStringSplit.java
+++ src/leetcode/string/BalancedStringSplit.java
@@ -1,31 +1,24 @@
 /**
+ * 1221. Split a String in Balanced Strings
+ *
  * Balanced strings are those that have an equal quantity of 'L' and 'R' characters.
-
-Given a balanced string s, split it in the maximum amount of balanced strings.
-
-Return the maximum amount of split balanced strings.
-
-Example 1:
-
-Input: s = "RLRRLLRLRL"
-Output: 4
-Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
-Example 2:
-
-Input: s = "RLLLLRRRLR"
-Output: 3
-Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
-Example 3:
-
-Input: s = "LLLLRRRR"
-Output: 1
-Explanation: s can be split into "LLLLRRRR".
-Example 4:
-
-Input: s = "RLRRRLLRLL"
-Output: 2
-Explanation: s can be split into "RL", "RRRLLRLL", since each substring contains an equal number of 'L' and 'R'
-
+ *
+ * Given a balanced string s, split it in the maximum amount of balanced strings.
+ *
+ * Return the maximum amount of split balanced strings.
+ *
+ * Example 1:
+ *
+ * Input: s = "RLRRLLRLRL" Output: 4 Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains
+ * same number of 'L' and 'R'. Example 2:
+ *
+ * Input: s = "RLLLLRRRLR" Output: 3 Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same
+ * number of 'L' and 'R'. Example 3:
+ *
+ * Input: s = "LLLLRRRR" Output: 1 Explanation: s can be split into "LLLLRRRR". Example 4:
+ *
+ * Input: s = "RLRRRLLRLL" Output: 2 Explanation: s can be split into "RL", "RRRLLRLL", since each substring contains an
+ * equal number of 'L' and 'R'
  */
 
 package leetcode.string;
@@ -34,18 +27,17 @@ public class BalancedStringSplit {
 
     public int balancedStringSplit(String s) {
         int counter = 0;
-        int res = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'L') {
+        int countOfBalancedString = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == 'R')
                 counter++;
-            } else if (s.charAt(i) == 'R') {
+            else if (ch == 'L')
                 counter--;
-            }
-            if (counter == 0) {
-                res++;
-            }
+
+            if (counter == 0)
+                countOfBalancedString++;
         }
-        return res;
+        return countOfBalancedString;
     }
 
     public static void main(String[] args) {
