72ced23c11542c910b7ba4dd10b713344519f238
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-07-10 10:12:24 -0400

    leetcode 2108

--- src/leetcode/string/FirstPalindrome.java
+++ src/leetcode/string/FirstPalindrome.java
@@ -1,33 +1,25 @@
 /**
- * 2108. Find First Palindromic String in the Array
-Easy
-
-139
-
-7
-
-Add to List
-
-Share
-Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".
-
-A string is palindromic if it reads the same forward and backward.
-
-Example 1:
-Input: words = ["abc","car","ada","racecar","cool"]
-Output: "ada"
-Explanation: The first string that is palindromic is "ada".
-Note that "racecar" is also palindromic, but it is not the first.
-
-Example 2:
-Input: words = ["notapalindrome","racecar"]
-Output: "racecar"
-Explanation: The first and only string that is palindromic is "racecar".
-
-Example 3:
-Input: words = ["def","ghi"]
-Output: ""
-Explanation: There are no palindromic strings, so the empty string is returned.
+ * 2108. Find First Palindromic String in the Array Easy
+ *
+ * 139
+ *
+ * 7
+ *
+ * Add to List
+ *
+ * Share Given an array of strings words, return the first palindromic string in the array. If there is no such string,
+ * return an empty string "".
+ *
+ * A string is palindromic if it reads the same forward and backward.
+ *
+ * Example 1: Input: words = ["abc","car","ada","racecar","cool"] Output: "ada" Explanation: The first string that is
+ * palindromic is "ada". Note that "racecar" is also palindromic, but it is not the first.
+ *
+ * Example 2: Input: words = ["notapalindrome","racecar"] Output: "racecar" Explanation: The first and only string that
+ * is palindromic is "racecar".
+ *
+ * Example 3: Input: words = ["def","ghi"] Output: "" Explanation: There are no palindromic strings, so the empty string
+ * is returned.
  */
 
 package leetcode.string;
@@ -35,27 +27,23 @@ package leetcode.string;
 public class FirstPalindrome {
 
     public String firstPalindrome(String[] words) {
-        if (words == null || words.length == 0) {
-            return "";
-        }
-        for (int i = 0; i < words.length; i++) {
-            if (validatePalindrome(words[i])) {
-                return words[i];
+        for (String word : words) {
+            if (isPalindrome(word)) {
+                return word;
             }
         }
         return "";
     }
 
-    public boolean validatePalindrome(String string) {
-        int i = 0;
-        int j = string.length() - 1;
-        while (i < j) {
-            if (string.charAt(i) != string.charAt(j)) {
+    public boolean isPalindrome(String string) {
+        int left = 0;
+        int right = string.length() - 1;
+        while (left < right) {
+            if (string.charAt(left) != string.charAt(right)) {
                 return false;
-            } else {
-                i++;
-                j--;
             }
+            left++;
+            right--;
         }
         return true;
     }
