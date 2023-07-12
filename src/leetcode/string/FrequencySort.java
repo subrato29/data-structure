ff81df1a82bbbdda5f2f98125d8a58bc0920abac
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-07-12 10:39:49 -0400

    leetcode 451

--- src/leetcode/string/FrequencySort.java
+++ src/leetcode/string/FrequencySort.java
@@ -25,26 +25,27 @@
 package leetcode.string;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.PriorityQueue;
 
 public class FrequencySort {
 
     public String frequencySort(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        for (char ch : s.toCharArray()) {
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
             map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
-        PriorityQueue<Character> pq = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
-        pq.addAll(map.keySet());
-        String finalString = "";
-        while (!pq.isEmpty()) {
-            char current = pq.poll();
+        PriorityQueue<Character> maxHeap = new PriorityQueue<Character>((a, b) -> map.get(b) - map.get(a));
+        maxHeap.addAll(map.keySet());
+
+        StringBuilder sb = new StringBuilder();
+        while (!maxHeap.isEmpty()) {
+            char current = maxHeap.poll();
             for (int i = 0; i < map.get(current); i++) {
-                finalString += current;
+                sb.append(current);
             }
         }
-        return finalString;
+        return sb.toString();
     }
 
     public static void main(String[] args) {
