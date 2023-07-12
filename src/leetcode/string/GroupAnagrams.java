58fc7b7c1b5d1be91a038bdeaf9bab949a3994b8
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-07-12 19:45:40 -0400

    leetcode 49

--- src/leetcode/string/GroupAnagrams.java
+++ src/leetcode/string/GroupAnagrams.java
@@ -1,54 +1,41 @@
 /**
- * 49. Group Anagrams
-Medium
-
-6163
-
-247
-
-Add to List
-
-Share
-Given an array of strings strs, group the anagrams together. You can return the answer in any order.
-
-An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
-
-Example 1:
-Input: strs = ["eat","tea","tan","ate","nat","bat"]
-Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
-
-Example 2:
-Input: strs = [""]
-Output: [[""]]
-
-Example 3:
-Input: strs = ["a"]
-Output: [["a"]]
+ * 49. Group Anagrams Medium
+ *
+ * 6163
+ *
+ * 247
+ *
+ * Add to List
+ *
+ * Share Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+ *
+ * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all
+ * the original letters exactly once.
+ *
+ * Example 1: Input: strs = ["eat","tea","tan","ate","nat","bat"] Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ *
+ * Example 2: Input: strs = [""] Output: [[""]]
+ *
+ * Example 3: Input: strs = ["a"] Output: [["a"]]
  */
 
 package leetcode.string;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class GroupAnagrams {
 
-    public List < List < String >> groupAnagrams(String[] strs) {
-        HashMap < String, List < String >> map = new HashMap < String, List < String >> ();
-        for (int i = 0; i < strs.length; i++) {
-            char[] strArr = strs[i].toCharArray();
-            Arrays.sort(strArr);
-            String sorted = new String(strArr);
-
-            if (map.keySet().contains(sorted)) {
-                map.get(sorted).add(strs[i]);
-            } else {
-                map.put(sorted, new ArrayList < String > (Arrays.asList(strs[i])));
-            }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] charArr = str.toCharArray();
+            Arrays.sort(charArr);
+            String sortedStr = new String(charArr);
+            map.putIfAbsent(sortedStr, new ArrayList<>());
+            map.get(sortedStr)
+                    .add(str);
         }
-        return new ArrayList < > (map.values());
+        return new ArrayList<>(map.values());
     }
 
     public static void main(String[] args) {
