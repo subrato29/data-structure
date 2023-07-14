55f61d6a30e839332a751b5a28219cc9682e7d97
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-07-14 10:09:13 -0400

    leetcode 28

--- src/leetcode/string/ImplementStrStr.java
+++ src/leetcode/string/ImplementStrStr.java
@@ -1,29 +1,37 @@
-/*
- * Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
-	Example 1:
-	Input: haystack = "hello", needle = "ll"
-	Output: 2
-	Example 2:
-	Input: haystack = "aaaaa", needle = "bba"
-	Output: -1
-	Clarification:
-	What should we return when needle is an empty string? This is a great question to ask during an interview.
-	For the purpose of this problem, we will return 0 when needle is an empty string. 
+/**
+ * 28. Find the Index of the First Occurrence in a String Easy 4K 219 Companies Given two strings needle and haystack,
+ * return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ *
+ * Example 1: Input: haystack = "sadbutsad", needle = "sad" Output: 0 Explanation: "sad" occurs at index 0 and 6. The
+ * first occurrence is at index 0, so we return 0.
+ *
+ * Example 2: Input: haystack = "leetcode", needle = "leeto" Output: -1 Explanation: "leeto" did not occur in
+ * "leetcode", so we return -1.
  */
 package leetcode.string;
 
 public class ImplementStrStr {
-	
-	public static int solution (String haystack, String needle) {
-		if (haystack.contains(needle)) {
-			return haystack.indexOf(needle);
-		} else {
-			return -1;
-		}
-	}
-	
-	public static void main(String[] args) {
-		System.out.println(solution("hello", "ll"));
-	}
+
+    public int strStr(String haystack, String needle) {
+        if (haystack.length() < needle.length()) {
+            return -1;
+        }
+        int haystackPointer = 0;
+        int needlePointer = 0;
+        while (haystackPointer < haystack.length() && needlePointer < needle.length()) {
+            if (haystack.charAt(haystackPointer) == needle.charAt(needlePointer)) {
+                needlePointer++;
+                if (needlePointer == needle.length()) {
+                    return haystackPointer - needlePointer + 1;
+                }
+            }
+            else {
+                haystackPointer = haystackPointer - needlePointer;
+                needlePointer = 0;
+            }
+            haystackPointer++;
+        }
+        return -1;
+    }
 
 }
