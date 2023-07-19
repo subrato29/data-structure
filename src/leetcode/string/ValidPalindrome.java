4b3e137585553d8b1dc89461c6f01a4eaf98cf23
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-07-19 14:07:09 -0400

    leetcode 123

--- src/leetcode/string/ValidPalindrome.java
+++ src/leetcode/string/ValidPalindrome.java
@@ -1,16 +1,19 @@
 /**
-Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
-
-Note: For the purpose of this problem, we define empty string as valid palindrome.
-
-Example 1:
-
-Input: "A man, a plan, a canal: Panama"
-Output: true
-Example 2:
-
-Input: "race a car"
-Output: false
+ * 125. Valid Palindrome
+ *
+ * Easy 7.4K 7.4K Companies A phrase is a palindrome if, after converting all uppercase letters into lowercase letters
+ * and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include
+ * letters and numbers.
+ *
+ * Given a string s, return true if it is a palindrome, or false otherwise.
+ *
+ * Example 1: Input: s = "A man, a plan, a canal: Panama" Output: true Explanation: "amanaplanacanalpanama" is a
+ * palindrome.
+ *
+ * Example 2: Input: s = "race a car" Output: false Explanation: "raceacar" is not a palindrome.
+ *
+ * Example 3: Input: s = " " Output: true Explanation: s is an empty string "" after removing non-alphanumeric
+ * characters. Since an empty string reads the same forward and backward, it is a palindrome.
  */
 package leetcode.string;
 
@@ -27,9 +30,11 @@ public class ValidPalindrome {
             char rightChar = string.charAt(right);
             if (!Character.isLetterOrDigit(leftChar)) {
                 left++;
-            } else if (!Character.isLetterOrDigit(rightChar)) {
+            }
+            else if (!Character.isLetterOrDigit(rightChar)) {
                 right--;
-            } else {
+            }
+            else {
                 if (Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) {
                     return false;
                 }
