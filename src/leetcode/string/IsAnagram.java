ba757a8e91cf9fd7a68e6bb90636ae8b00159358
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-07-18 23:39:49 -0400

    leetcode 242

--- src/leetcode/string/IsAnagram.java
+++ src/leetcode/string/IsAnagram.java
@@ -1,28 +1,23 @@
 /**
- * 242. Valid Anagram
-Easy
-
-3191
-
-176
-
-Add to List
-
-Share
-Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-
-Example 1:
-Input: s = "anagram", t = "nagaram"
-Output: true
-
-Example 2:
-Input: s = "rat", t = "car"
-Output: false
+ * 242. Valid Anagram Easy
+ *
+ * 3191
+ *
+ * 176
+ *
+ * Add to List
+ *
+ * Share Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+ *
+ * Example 1: Input: s = "anagram", t = "nagaram" Output: true
+ *
+ * Example 2: Input: s = "rat", t = "car" Output: false
  */
 
 package leetcode.string;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class IsAnagram {
 
@@ -30,17 +25,15 @@ public class IsAnagram {
         if (s.length() != t.length()) {
             return false;
         }
-        HashMap < Character, Integer > map = new HashMap < Character, Integer > ();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : s.toCharArray()) {
             map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
-        for (int i = 0; i < t.length(); i++) {
-            char ch = t.charAt(i);
+        for (char ch : t.toCharArray()) {
             if (!map.containsKey(ch) || map.get(ch) == 0) {
                 return false;
             }
-            map.put(ch, map.get(ch) - 1);
+            map.put(ch, map.getOrDefault(ch, 0) - 1);
         }
         return true;
     }
