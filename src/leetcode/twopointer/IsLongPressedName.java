af92754fc298ab38d073808937618e9f55485ff8
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-08-04 12:26:24 -0400

    leetcode 925

--- src/leetcode/twopointer/IsLongPressedName.java
+++ src/leetcode/twopointer/IsLongPressedName.java
@@ -1,50 +1,50 @@
 /**
- * 925. Long Pressed Name
-Easy
-
-1527
-
-234
-
-Add to List
-
-Share
-Your friend is typing his name into a keyboard. Sometimes, when typing a character c, the key might get long pressed, and the character will be typed 1 or more times.
-
-You examine the typed characters of the keyboard. Return True if it is possible that it was your friends name, with some characters (possibly none) being long pressed.
-
-Example 1:
-Input: name = "alex", typed = "aaleex"
-Output: true
-Explanation: 'a' and 'e' in 'alex' were long pressed.
-
-Example 2:
-Input: name = "saeed", typed = "ssaaedd"
-Output: false
-Explanation: 'e' must have been pressed twice, but it was not in the typed output.
+ * 925. Long Pressed Name Easy
+ *
+ * 1527
+ *
+ * 234
+ *
+ * Add to List
+ *
+ * Share Your friend is typing his name into a keyboard. Sometimes, when typing a character c, the key might get long
+ * pressed, and the character will be typed 1 or more times.
+ *
+ * You examine the typed characters of the keyboard. Return True if it is possible that it was your friends name, with
+ * some characters (possibly none) being long pressed.
+ *
+ * Example 1: Input: name = "alex", typed = "aaleex" Output: true Explanation: 'a' and 'e' in 'alex' were long pressed.
+ *
+ * Example 2: Input: name = "saeed", typed = "ssaaedd" Output: false Explanation: 'e' must have been pressed twice, but
+ * it was not in the typed output.
  */
 package leetcode.twopointer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class IsLongPressedName {
 
     public boolean isLongPressedName(String name, String typed) {
-        if (name.length() > typed.length()) {
-            return false;
-        }
-        int i = 0;
-        int j = 0;
-        while (j < typed.length()) {
-            if (i < name.length() && name.charAt(i) == typed.charAt(j)) {
-                i++;
-            } else if (j == 0 || typed.charAt(j) != typed.charAt(j - 1)) {
+        int nameIndex = 0;
+        int typedIndex = 0;
+
+        while (typedIndex < typed.length()) {
+            if (nameIndex < name.length() && name.charAt(nameIndex) == typed.charAt(typedIndex)) {
+                nameIndex++;
+                typedIndex++;
+            }
+            else if (typedIndex > 0 && typed.charAt(typedIndex) == typed.charAt(typedIndex - 1)) {
+                typedIndex++;
+            }
+            else {
                 return false;
             }
-            j++;
         }
-        return i == name.length();
+        return nameIndex == name.length();
     }
+
     public static void main(String[] args) {
-        
     }
 
 }
