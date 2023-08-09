9dd44211fe14a3f4240916e95b8d4c023b41c574
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-08-09 14:01:59 -0400

    leetcode 3

--- src/leetcode/twopointer/LengthOfLongestSubstring.java
+++ src/leetcode/twopointer/LengthOfLongestSubstring.java
@@ -1,55 +1,43 @@
 /**
  * 3. Longest Substring Without Repeating Characters
-Medium
-
-22101
-
-993
-
-Add to List
-
-Share
-Given a string s, find the length of the longest substring without repeating characters.
-
-Example 1:
-Input: s = "abcabcbb"
-Output: 3
-Explanation: The answer is "abc", with the length of 3.
-
-Example 2:
-Input: s = "bbbbb"
-Output: 1
-Explanation: The answer is "b", with the length of 1.
-
-Example 3:
-Input: s = "pwwkew"
-Output: 3
-Explanation: The answer is "wke", with the length of 3.
-Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ *
+ * Medium 36.1K 1.6K Companies
+ *
+ * Given a string s, find the length of the longest substring without repeating characters.
+ *
+ * Example 1: Input: s = "abcabcbb" Output: 3 Explanation: The answer is "abc", with the length of 3.
+ *
+ * Example 2: Input: s = "bbbbb" Output: 1 Explanation: The answer is "b", with the length of 1.
+ *
+ * Example 3: Input: s = "pwwkew" Output: 3 Explanation: The answer is "wke", with the length of 3. Notice that the
+ * answer must be a substring, "pwke" is a subsequence and not a substring.
  */
 package leetcode.twopointer;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class LengthOfLongestSubstring {
 
     public int lengthOfLongestSubstring(String s) {
-        int unique_char_pointer = 0;
-        int duplicate_char_pointer = 0;
-        int max = 0;
-        HashSet < Character > set = new HashSet < Character > ();
-        while (unique_char_pointer < s.length()) {
-            char ch = s.charAt(unique_char_pointer);
+        Set<Character> set = new HashSet<>();
+        int maxLength = 0;
+        int uniqueCharindex = 0;
+        int duplicateCharIndex = 0;
+
+        while (uniqueCharindex < s.length()) {
+            char ch = s.charAt(uniqueCharindex);
             if (!set.contains(ch)) {
                 set.add(ch);
-                unique_char_pointer++;
-                max = Math.max(set.size(), max);
-            } else {
-                set.remove(s.charAt(duplicate_char_pointer));
-                duplicate_char_pointer++;
+                uniqueCharindex++;
+                maxLength = Math.max(set.size(), maxLength);
+            }
+            else {
+                set.remove(s.charAt(duplicateCharIndex));
+                duplicateCharIndex++;
             }
         }
-        return max;
+        return maxLength;
     }
 
     public static void main(String[] args) {
