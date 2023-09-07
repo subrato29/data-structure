5701a8922cd8eb6406e1a7802a8be9fde91b3638
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-09-07 17:00:31 -0400

    leetcode 767

--- src/leetcode/twopointer/ReorganizeString.java
+++ src/leetcode/twopointer/ReorganizeString.java
@@ -1,25 +1,19 @@
 /**
- * 767. Reorganize String
-Medium
-
-3749
-
-162
-
-Add to List
-
-Share
-Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
-
-Return any possible rearrangement of s or return "" if not possible.
-
-Example 1:
-Input: s = "aab"
-Output: "aba"
-
-Example 2:
-Input: s = "aaab"
-Output: ""
+ * 767. Reorganize String Medium
+ *
+ * 3749
+ *
+ * 162
+ *
+ * Add to List
+ *
+ * Share Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
+ *
+ * Return any possible rearrangement of s or return "" if not possible.
+ *
+ * Example 1: Input: s = "aab" Output: "aba"
+ *
+ * Example 2: Input: s = "aaab" Output: ""
  */
 package leetcode.twopointer;
 
@@ -29,40 +23,39 @@ import java.util.PriorityQueue;
 public class ReorganizeString {
 
     public String reorganizeString(String s) {
-        HashMap < Character, Integer > map = new HashMap < Character, Integer > ();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        HashMap<Character, Integer> charWiseFreq = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            charWiseFreq.put(ch, charWiseFreq.getOrDefault(ch, 0) + 1);
         }
-        @SuppressWarnings({
-            "unchecked",
-            "rawtypes"
-        })
-        PriorityQueue < Character > maxHeap = new PriorityQueue((a, b) -> map.get(b) - map.get(a));
-        maxHeap.addAll(map.keySet());
+        PriorityQueue<Character> maxHeap = new PriorityQueue((a, b) -> charWiseFreq.get(b) - charWiseFreq.get(a));
+        maxHeap.addAll(charWiseFreq.keySet());
 
         StringBuilder sb = new StringBuilder();
-        while (maxHeap.size() > 1) {
+        while (maxHeap.size() >= 2) {
             char curr = maxHeap.remove();
             char next = maxHeap.remove();
+
             sb.append(curr);
             sb.append(next);
-            map.put(curr, map.get(curr) - 1);
-            map.put(next, map.get(next) - 1);
-            if (map.get(curr) > 0) {
+
+            charWiseFreq.put(curr, charWiseFreq.get(curr) - 1);
+            charWiseFreq.put(next, charWiseFreq.get(next) - 1);
+
+            if (charWiseFreq.get(curr) > 0) {
                 maxHeap.add(curr);
             }
-            if (map.get(next) > 0) {
+            if (charWiseFreq.get(next) > 0) {
                 maxHeap.add(next);
             }
         }
         while (!maxHeap.isEmpty()) {
             char last = maxHeap.remove();
-            if (map.get(last) > 1) {
+            if (charWiseFreq.get(last) > 1) {
                 return "";
             }
             sb.append(last);
         }
+
         return sb.toString();
     }
 
