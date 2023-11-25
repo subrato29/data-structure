9e77bca8b3c453f49ae0c95f06c2fa9870fa21e0
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2023-11-24 20:40:54 -0500

    pascal triangle

--- src/leetcode/nickWhite/Pascals_Triangle_118.java
+++ src/leetcode/nickWhite/Pascals_Triangle_118.java
@@ -1,33 +1,35 @@
 /**
  * 118. Pascal's Triangle
-Easy
+ Easy
 
-6345
+ 6345
 
-221
+ 221
 
-Add to List
+ Add to List
 
-Share
-Given an integer numRows, return the first numRows of Pascal's triangle.
+ Share
+ Given an integer numRows, return the first numRows of Pascal's triangle.
 
-In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+ In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
 
 
-Example 1:
-Input: numRows = 5
-Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+ Example 1:
+ Input: numRows = 5
+ Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
 
-Example 2:
-Input: numRows = 1
-Output: [[1]]
+ Example 2:
+ Input: numRows = 1
+ Output: [[1]]
  */
-package leetcode.easy;
+
+package leetcode.nickWhite;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Pascals_Triangle_118 {
+
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> triangle = new ArrayList<>();
         List<Integer> firstRow = new ArrayList<>();
@@ -50,10 +52,4 @@ public class Pascals_Triangle_118 {
 
         return triangle;
     }
-
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
-    }
-
 }
