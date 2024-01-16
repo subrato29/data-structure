59343fcb3dcf795832a6aae301cf82e94861ff33
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2024-01-16 14:43:56 -0500

    leetcode 152

---
+++ src/leetcode/neetcode/leetcode_blind_75_solutions/Maximum_Product_Subarray_152.java
@@ -0,0 +1,43 @@
+/**
+ * 152. Maximum Product Subarray
+ *
+ * Given an integer array nums, find a subarray that has the largest product, and return the product.
+ *
+ * The test cases are generated so that the answer will fit in a 32-bit integer.
+ *
+ * Example 1:
+ * Input: nums = [2,3,-2,4]
+ * Output: 6
+ * Explanation: [2,3] has the largest product 6.
+ *
+ * Example 2:
+ * Input: nums = [-2,0,-1]
+ * Output: 0
+ * Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+ */
+
+package leetcode.neetcode.leetcode_blind_75_solutions;
+
+public class Maximum_Product_Subarray_152 {
+
+    public int maxProduct(int[] nums) {
+        int maxProduct = nums[0];
+        int minProduct = nums[0];
+        int res = maxProduct;
+
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] < 0) {
+                int temp = maxProduct;
+                maxProduct = minProduct;
+                minProduct = temp;
+            }
+
+            maxProduct = Math.max(nums[i], nums[i] * maxProduct);
+            minProduct = Math.min(nums[i], nums[i] * minProduct);
+
+            res = Math.max(res, maxProduct);
+        }
+
+        return res;
+    }
+}
