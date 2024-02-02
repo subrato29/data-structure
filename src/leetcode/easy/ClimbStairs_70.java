9d65efd607f2c8a3c00cc3bd1bc3264cb1a0b0ab
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2024-02-02 15:06:52 -0500

    leetcode 70

---
+++ src/leetcode/easy/ClimbStairs_70.java
@@ -0,0 +1,54 @@
+/**
+ * 70. Climbing Stairs
+Easy
+
+9401
+
+290
+
+Add to List
+
+Share
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Example 2:
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ */
+
+package leetcode.easy;
+
+public class ClimbStairs_70 {
+
+    public int climbStairs(int n) {
+        int topStair = 1;
+        int secondTopStair = 1;
+
+        for(int i = 0; i < n - 1; i++) {
+            int temp = topStair;
+            topStair += secondTopStair;
+            secondTopStair = temp;
+        }
+
+        return topStair;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
