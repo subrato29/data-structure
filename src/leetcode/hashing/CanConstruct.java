fe6d58a2ca10cd3fbed0466a3cead01a61e4dcfb
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2024-12-09 22:21:08 -0500

    383. Ransom Note

--- src/leetcode/hashing/CanConstruct.java
+++ src/leetcode/hashing/CanConstruct.java
@@ -1,3 +1,5 @@
+package src.leetcode.hashing;
+
 /**
  * 383. Ransom Note
 Easy
@@ -25,26 +27,26 @@ Example 3:
 Input: ransomNote = "aa", magazine = "aab"
 Output: true
  */
-package leetcode.hashing;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class CanConstruct {
 
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap < Character, Integer > map = new HashMap < Character, Integer > ();
-        for (int i = 0; i < magazine.length(); i++) {
-            char ch = magazine.charAt(i);
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(char ch : magazine.toCharArray()) {
             map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
 
-        for (int i = 0; i < ransomNote.length(); i++) {
-            char ch = ransomNote.charAt(i);
-            if (!map.containsKey(ch) || map.get(ch) == 0) {
+        for(char ch : ransomNote.toCharArray()) {
+            if(!map.containsKey(ch) || map.get(ch) == 0) {
                 return false;
             }
             map.put(ch, map.get(ch) - 1);
         }
+
         return true;
     }
 
