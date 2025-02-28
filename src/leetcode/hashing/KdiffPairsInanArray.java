06a48db981687b8417f5f27035130e1a199db3d3
Subrato Sarkar <ssarkar@SSARKAR2-MBP16.local>
2025-02-27 23:30:27 -0500

    leetcode 532

--- src/leetcode/hashing/KdiffPairsInanArray.java
+++ src/leetcode/hashing/KdiffPairsInanArray.java
@@ -1,51 +1,65 @@
 /**
- * Given an array of integers and an integer k, you
- * need to find the number of unique k-diff pairs in the array. Here a k-diff pair is defined as an
- * integer pair (i, j), where i and j are both numbers in the array and their absolute difference is
- * k.
+ * 532. K-diff Pairs in an Array
+ * Medium
+ * Topics
+ * Companies
+ * Given an array of integers nums and an integer k, return the number of unique k-diff pairs in the array.
  *
- * <p>Example 1: Input: [3, 1, 4, 1, 5], k = 2 Output: 2 Explanation: There are two 2-diff pairs in
- * the array, (1, 3) and (3, 5). Although we have two 1s in the input, we should only return the
- * number of unique pairs. Example 2: Input:[1, 2, 3, 4, 5], k = 1 Output: 4 Explanation: There are
- * four 1-diff pairs in the array, (1, 2), (2, 3), (3, 4) and (4, 5). Example 3: Input: [1, 3, 1, 5,
- * 4], k = 0 Output: 1 Explanation: There is one 0-diff pair in the array, (1, 1). Note: The pairs
- * (i, j) and (j, i) count as the same pair. The length of the array won't exceed 10,000. All the
- * integers in the given input belong to the range: [-1e7, 1e7].
+ * A k-diff pair is an integer pair (nums[i], nums[j]), where the following are true:
+ *
+ * 0 <= i, j < nums.length
+ * i != j
+ * |nums[i] - nums[j]| == k
+ * Notice that |val| denotes the absolute value of val.
+ *
+ *
+ *
+ * Example 1:
+ * Input: nums = [3,1,4,1,5], k = 2
+ * Output: 2
+ * Explanation: There are two 2-diff pairs in the array, (1, 3) and (3, 5).
+ * Although we have two 1s in the input, we should only return the number of unique pairs.
+ *
+ * Example 2:
+ * Input: nums = [1,2,3,4,5], k = 1
+ * Output: 4
+ * Explanation: There are four 1-diff pairs in the array, (1, 2), (2, 3), (3, 4) and (4, 5).
+ *
+ * Example 3:
+ * Input: nums = [1,3,1,5,4], k = 0
+ * Output: 1
+ * Explanation: There is one 0-diff pair in the array, (1, 1).
  */
-package leetcode.hashing;
-import java.util.ArrayList;
-import java.util.List;
+
+package src.leetcode.hashing;
+import java.util.HashMap;
+import java.util.Map;
 
 public class KdiffPairsInanArray {
 
-    public static int solution(int[] array, int k) {
-        if (array.length == 0) {
-            return 0;
+    public int findPairs(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int counter = 0;
+
+        for(int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        List < Integer > list = new ArrayList < Integer > ();
-        int count = 0;
-        for (int i = 0; i < array.length; i++) {
-            list.add(array[i]);
-        }
-        for (int i = 0; i < array.length; i++) {
-            int complement = Math.abs(array[i] - k);
-            if (list.contains(complement) && (array[i] != complement)) {
-                System.out.println(array[i] + "===" + complement);
-                count++;
+
+        for(int num : map.keySet()) {
+            if(k == 0) {
+                if(map.get(num) > 1) {
+                    counter++;
+                }
+            }else if(map.containsKey(num + k)) {
+                counter++;
             }
         }
-        return count;
+
+        return counter;
     }
 
     public static void main(String[] args) {
-        int[] array = {
-            1,
-            3,
-            1,
-            5,
-            4
-        };
-        System.out.println(solution(array, 2));
+
     }
 
 }
