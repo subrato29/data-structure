590773446cd3e2666c151e70fec92fe9dc682bf0
subrato29 <subratosarkar29@gmail.com>
2025-08-22 11:12:29 -0400

    leetcode 1674

--- src/leetcode/string/MaxLengthBetweenEqualCharacters.java
+++ src/leetcode/string/MaxLengthBetweenEqualCharacters.java
@@ -1,9 +1,5 @@
-package leetcode.string;
-
-import java.util.HashMap;
 /**
  * 1624. Largest Substring Between Two Equal Characters
- * 
  * Given a string s, return the length of the longest substring between two equal characters, excluding the two characters. If there is no such substring return -1.
 
 A substring is a contiguous sequence of characters within a string.
@@ -28,6 +24,10 @@ Input: s = "cabbac"
 Output: 4
 Explanation: The optimal substring here is "abba". Other non-optimal substrings include "bb" and "".
  */
+
+package src.leetcode.string;
+
+import java.util.HashMap;
 import java.util.Map;
 
 public class MaxLengthBetweenEqualCharacters {
