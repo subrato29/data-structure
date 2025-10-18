839c74e483108ce52a853911ca146713060c74b2
subrato29 <subratosarkar29@gmail.com>
2025-10-18 12:26:44 -0400

    Leetcode 921

--- src/leetcode/string/MinimumAddtoMakeParenthesesValid.java
+++ src/leetcode/string/MinimumAddtoMakeParenthesesValid.java
@@ -1,31 +1,28 @@
 /**
- * Given a string S of '(' and ')' parentheses, we add the minimum number of parentheses ( '(' or ')', and in any positions ) so that the resulting parentheses string is valid.
-
-Formally, a parentheses string is valid if and only if:
-
-It is the empty string, or
-It can be written as AB (A concatenated with B), where A and B are valid strings, or
-It can be written as (A), where A is a valid string.
-Given a parentheses string, return the minimum number of parentheses we must add to make the resulting string valid.
-
-
-Example 1:
-Input: "())"
-Output: 1
-
-Example 2:
-Input: "((("
-Output: 3
-
-Example 3:
-Input: "()"
-Output: 0
-
-Example 4:
-Input: "()))(("
-Output: 4
+ * 921. Minimum Add to Make Parentheses Valid
+ * Medium
+ *
+ * A parentheses string is valid if and only if:
+ *
+ * It is the empty string,
+ * It can be written as AB (A concatenated with B), where A and B are valid strings, or
+ * It can be written as (A), where A is a valid string.
+ * You are given a parentheses string s. In one move, you can insert a parenthesis at any position of the string.
+ *
+ * For example, if s = "()))", you can insert an opening parenthesis to be "(()))" or a closing parenthesis to be "())))".
+ * Return the minimum number of moves required to make s valid.
+ *
+ * Example 1:
+ * Input: s = "())"
+ * Output: 1
+ *
+ * Example 2:
+ * Input: s = "((("
+ * Output: 3
  */
-package leetcode.string;
+
+
+package src.leetcode.string;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -43,7 +40,7 @@ public class MinimumAddtoMakeParenthesesValid {
             char curr = input.charAt(i);
             if (map.containsKey(curr)) {
                 stack.push(curr);
-            } else if (map.values().contains(curr)) {
+            } else if (map.containsValue(curr)) {
                 if (!stack.empty() && map.get(stack.peek()) == curr) {
                     stack.pop();
                 } else {
